236c7266b9ddb3b79b7490025964bd469af475d9
Peter Keenan <keenan@posteo.net>
2021-08-17 01:12:36 +0100

    Code following format clean

--- hamming/Hamming.fs
+++ hamming/Hamming.fs
@@ -1,9 +1,10 @@
-﻿module Hamming
+module Hamming
 
- let distance (strand1:string) (strand2:string) =
-     match strand1.Length = strand2.Length with
-     | true -> 
-        Seq.map2((=)) strand1 strand2 
-        |> Seq.sumBy(fun isMatch -> if isMatch then 0 else 1)
+let distance (strand1: string) (strand2: string) =
+    match strand1.Length = strand2.Length with
+    | true ->
+        Seq.map2 ((=)) strand1 strand2
+        |> Seq.sumBy (fun isMatch ->
+            if isMatch then 0 else 1)
         |> Some
-     | false -> None
+    | false -> None
--- hamming/HammingTests.fs
+++ hamming/HammingTests.fs
@@ -8,38 +8,28 @@ open Xunit
 open Hamming
 
 [<Fact>]
-let ``Empty strands`` () =
-    distance "" "" |> should equal (Some 0)
+let ``Empty strands``() = distance "" "" |> should equal (Some 0)
 
 [<Fact>]
-let ``Single letter identical strands`` () =
-    distance "A" "A" |> should equal (Some 0)
+let ``Single letter identical strands``() = distance "A" "A" |> should equal (Some 0)
 
 [<Fact>]
-let ``Single letter different strands`` () =
-    distance "G" "T" |> should equal (Some 1)
+let ``Single letter different strands``() = distance "G" "T" |> should equal (Some 1)
 
 [<Fact>]
-let ``Long identical strands`` () =
-    distance "GGACTGAAATCTG" "GGACTGAAATCTG" |> should equal (Some 0)
+let ``Long identical strands``() = distance "GGACTGAAATCTG" "GGACTGAAATCTG" |> should equal (Some 0)
 
 [<Fact>]
-let ``Long different strands`` () =
-    distance "GGACGGATTCTG" "AGGACGGATTCT" |> should equal (Some 9)
+let ``Long different strands``() = distance "GGACGGATTCTG" "AGGACGGATTCT" |> should equal (Some 9)
 
 [<Fact>]
-let ``Disallow first strand longer`` () =
-    distance "AATG" "AAA" |> should equal None
+let ``Disallow first strand longer``() = distance "AATG" "AAA" |> should equal None
 
 [<Fact>]
-let ``Disallow second strand longer`` () =
-    distance "ATA" "AGTG" |> should equal None
+let ``Disallow second strand longer``() = distance "ATA" "AGTG" |> should equal None
 
 [<Fact>]
-let ``Disallow left empty strand`` () =
-    distance "" "G" |> should equal None
+let ``Disallow left empty strand``() = distance "" "G" |> should equal None
 
 [<Fact>]
-let ``Disallow right empty strand`` () =
-    distance "G" "" |> should equal None
-
+let ``Disallow right empty strand``() = distance "G" "" |> should equal None
