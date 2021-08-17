b4787f63c483ea5f20f6cbeca9a223bc8acd88fb
Peter Keenan <keenan@posteo.net>
2021-08-17 18:35:11 +0100

    Logic for standardising string input

--- pangram/Pangram.fs
+++ pangram/Pangram.fs
@@ -1,6 +1,14 @@
 ﻿module Pangram
 
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+let handleStringFormatting (string: string) =
+    string.Trim().ToLower()
+//    |> string.Split
+    |> Seq.sort
+    |> Seq.filter (fun (x) -> x.ToString() <> " ")
+    |> Seq.toList
+
 let isPangram (input: string): bool =
     match input <> "" with
     | true -> true
-    | false -> input.Contains("abcx")
