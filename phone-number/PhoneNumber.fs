e37c643d96aabb5cb8b1216f7db36051e0eb02eb
Peter Keenan <keenan@posteo.net>
2021-08-25 08:40:42 +0100

    Minor progress, but still not working

--- phone-number/PhoneNumber.fs
+++ phone-number/PhoneNumber.fs
@@ -1,14 +1,20 @@
 ﻿module PhoneNumber
 
-let strToNum numbers = int numbers
+let strToNum (numbers: string) = uint64 numbers
+
+// strToNum "0123"
 
 
 
-let clean (input: string) =
+let clean input =
     let numbers = input |> Seq.filter System.Char.IsNumber
     let bigNumAsString = numbers.ToString()
+
 
 
     match bigNumAsString.Length with
     | 10 -> strToNum bigNumAsString
     | _ -> strToNum bigNumAsString
+
+
+let result = clean "(223) 456-7890"
