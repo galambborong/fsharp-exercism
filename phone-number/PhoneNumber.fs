0e77dc4b750f7c898936cf0a4c72333463d47393
Peter Keenan <keenan@posteo.net>
2021-08-27 22:44:57 +0100

    Clean up pipeline and correctly return converted uint64

--- phone-number/PhoneNumber.fs
+++ phone-number/PhoneNumber.fs
@@ -1,20 +1,10 @@
 ﻿module PhoneNumber
 
-let strToNum (numbers: string) = uint64 numbers
-
-// strToNum "0123"
-
-
+open System
 
 let clean input =
-    let numbers = input |> Seq.filter System.Char.IsNumber
-    let bigNumAsString = numbers.ToString()
-
-
-
-    match bigNumAsString.Length with
-    | 10 -> strToNum bigNumAsString
-    | _ -> strToNum bigNumAsString
-
-
-let result = clean "(223) 456-7890"
+    input
+    |> Seq.filter Char.IsNumber
+    |> Seq.map string
+    |> String.concat ""
+    |> uint64
