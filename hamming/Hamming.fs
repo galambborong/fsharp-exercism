7986518f9d9dcce4791848c368689fdc0b4120fe
Peter Keenan <keenan@posteo.net>
2021-08-28 11:07:54 +0100

    Remove redundant brackets from Hamming solution

--- hamming/Hamming.fs
+++ hamming/Hamming.fs
@@ -3,7 +3,7 @@ module Hamming
 let distance (strand1: string) (strand2: string) =
     match strand1.Length = strand2.Length with
     | true ->
-        Seq.map2 ((=)) strand1 strand2
+        Seq.map2 (=) strand1 strand2
         |> Seq.sumBy (fun isMatch ->
             if isMatch then 0 else 1)
         |> Some
