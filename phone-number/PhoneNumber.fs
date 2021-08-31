4a9ec7019aa08788cbd7245734261f136322e4cf
Peter Keenan <keenan@posteo.net>
2021-08-31 23:26:20 +0100

    Truncate pattern match outcomes to single lines

--- phone-number/PhoneNumber.fs
+++ phone-number/PhoneNumber.fs
@@ -19,13 +19,9 @@ let clean input =
     | n, _, _ when n.Length = 11 && n.[0] <> '1' -> Error "11 digits must start with 1"
     | n, _, _ when n.Length < 10 -> Error "incorrect number of digits"
     | n, _, _ when n.Length > 11 -> Error "more than 11 digits"
-    | n, _, _ when n.StartsWith('0') -> Error "area code cannot start with zero"
-    | n, _, _ when n.Length = 11 && n.[1] = '0' -> Error "area code cannot start with zero"
-    | n, _, _ when n.Length <> 11 && n.[0] = '1' -> Error "area code cannot start with one"
-    | n, _, _ when n.Length = 11 && n.[1] = '1' -> Error "area code cannot start with one"
-    | n, _, _ when n.Length = 10 && n.[3] = '0' -> Error "exchange code cannot start with zero"
-    | n, _, _ when n.Length = 10 && n.[3] = '1' -> Error "exchange code cannot start with one"
-    | n, _, _ when n.Length = 11 && n.[4] = '0' -> Error "exchange code cannot start with zero"
-    | n, _, _ when n.Length = 11 && n.[4] = '1' -> Error "exchange code cannot start with one"
+    | n, _, _ when n.StartsWith('0') || n.Length = 11 && n.[1] = '0' -> Error "area code cannot start with zero"
+    | n, _, _ when n.Length <> 11 && n.[0] = '1' || n.Length = 11 && n.[1] = '1' -> Error "area code cannot start with one"
+    | n, _, _ when n.Length = 10 && n.[3] = '0' || n.Length = 11 && n.[4] = '0' -> Error "exchange code cannot start with zero"
+    | n, _, _ when n.Length = 10 && n.[3] = '1' || n.Length = 11 && n.[4] = '1' -> Error "exchange code cannot start with one"
     | n, _, _ when n.Length = 11 && n.[0] = '1' -> parseToInt (n.Substring(1, 10))
     | _ -> parseToInt nums
