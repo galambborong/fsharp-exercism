8f7d0519ce1d1559f01054555f2ab54f34c4ce49
Peter Keenan <keenan@posteo.net>
2021-09-04 09:30:10 +0100

    Lasagna kata solution

--- lucians-luscious-lasagna/LuciansLusciousLasagna.fs
+++ lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -1,9 +1,10 @@
 module LuciansLusciousLasagna
 
-// TODO: define the 'expectedMinutesInOven' binding
+let expectedMinutesInOven = 40
 
-// TODO: define the 'remainingMinutesInOven' function
+let remainingMinutesInOven timeInOven = expectedMinutesInOven - timeInOven
 
-// TODO: define the 'preparationTimeInMinutes' function
+let preparationTimeInMinutes layers = layers * 2
 
-// TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes layers timeInOven =
+    preparationTimeInMinutes layers + timeInOven
