c4a2d91c17392fae397a7a2389304511e8e50813
Peter Keenan <keenan@posteo.net>
2021-09-11 11:32:21 +0100

    Completed up to end of task 2

--- annalyns-infiltration/AnnalynsInfiltration.fs
+++ annalyns-infiltration/AnnalynsInfiltration.fs
@@ -1,9 +1,15 @@
 module AnnalynsInfiltration
 
-let canFastAttack (knightIsAwake: bool): bool = failwith "Please implement the 'canFastAttack' function"
+let canFastAttack knightIsAwake =
+    match knightIsAwake with
+    | true -> false
+    | false -> true
 
-let canSpy(knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool): bool =
-  failwith "Please implement the 'canSpy' function"
+let canSpy knightIsAwake archerIsAwake prisonerIsAwake =
+    match knightIsAwake, archerIsAwake, prisonerIsAwake with
+    | false, false, false -> false
+    | _ -> true
+    
 
 let canSignalPrisoner(archerIsAwake: bool) (prisonerIsAwake: bool): bool =
    failwith "Please implement the 'canSignalPrisoner' function"
