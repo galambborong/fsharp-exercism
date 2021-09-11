a6e5e7e632a3f30d379c8d52bdac8599b6d97f23
Peter Keenan <keenan@posteo.net>
2021-09-11 18:36:50 +0100

    Working solution pre tidy

--- annalyns-infiltration/AnnalynsInfiltration.fs
+++ annalyns-infiltration/AnnalynsInfiltration.fs
@@ -11,8 +11,17 @@ let canSpy knightIsAwake archerIsAwake prisonerIsAwake =
     | _ -> true
     
 
-let canSignalPrisoner(archerIsAwake: bool) (prisonerIsAwake: bool): bool =
-   failwith "Please implement the 'canSignalPrisoner' function"
+let canSignalPrisoner archerIsAwake prisonerIsAwake =
+    match archerIsAwake, prisonerIsAwake with
+    | false, true -> true
+    | _ -> false
 
-let canFreePrisoner(knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool) (petDogIsPresent: bool): bool =
- failwith "Please implement the 'canFreePrisoner' function"
+let canFreePrisoner knightIsAwake archerIsAwake prisonerIsAwake petDogIsPresent =
+    match knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent with
+    | true, true, true, _ -> false
+    | false, true, true, _ -> false
+    | true, false, true, false -> false
+    | _, _, true, _ -> true
+    | _, _, _, false -> false
+    | _, false, _, true -> true
+    | _, true, _, true -> false
