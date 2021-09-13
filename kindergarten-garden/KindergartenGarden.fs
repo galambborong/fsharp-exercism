58830d0062bae6cba93630a229da20b76b307277
Peter Keenan <keenan@posteo.net>
2021-09-13 08:45:53 +0100

    Kindergarten kata

---
+++ kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,17 @@
+﻿module KindergartenGarden
+
+type Plant =
+    | Grass
+    | Clover
+    | Radishes
+    | Violets
+
+let mapPlant x =
+      match x with
+      | "R" -> Plant.Radishes
+      | "C" -> Plant.Clover
+      | "G" -> Plant.Grass
+      | "V" -> Plant.Violets
+      
+let plants (diagram: string) student =
+    diagram.Split('\n')
