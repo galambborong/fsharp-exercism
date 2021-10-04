8eb1a3f55a9f4926ae31f1fadc9681f4e95b659a
Peter Keenan <keenan@posteo.net>
2021-10-04 21:34:25 +0100

    First solution with all tests passing

--- tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
+++ tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
@@ -1,14 +1,26 @@
 module TracksOnTracksOnTracks
 
-let newList: string list = failwith "Please implement the 'newList' value"
+let newList: string list = []
 
-let existingList: string list = failwith "Please implement the 'existingList' value"
+let existingList = [ "F#"; "Clojure"; "Haskell" ]
 
-let addLanguage (language: string) (languages: string list): string list =
-    failwith "Please implement the 'addLanguage' function"
+let addLanguage language languagesList = language :: languagesList
 
-let countLanguages (languages: string list): int = failwith "Please implement the 'countLanguages' function"
+let countLanguages (list: string list) = list.Length
 
-let reverseList(languages: string list): string list = failwith "Please implement the 'reverseList' function"
+let reverseList (languages: string list) : string list = languages |> List.rev
 
-let excitingList (languages: string list): bool = failwith "Please implement the 'excitingList' function"
+let excitingList (languages: string list) : bool =
+    let languageCount = countLanguages languages
+
+    match languages, languageCount with
+    | _, c when c = 0 -> false
+    | l, c when c = 1 || c > 3 ->
+        match l.Head with
+        | "F#" -> true
+        | _ -> false
+    | l, c when c < 4 ->
+        match l.[0..1] with
+        | l when l.Head = "F#" || l.[1] = "F#" -> true
+        | _ -> false
+    | _ -> false
