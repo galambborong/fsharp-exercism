1b5c738301a493d18da474c44e20498debe4e169
Peter Keenan <keenan@posteo.net>
2021-10-05 22:11:33 +0100

    First solution for acronym

--- acronym/Acronym.fs
+++ acronym/Acronym.fs
@@ -1,3 +1,13 @@
 ﻿module Acronym
 
-let abbreviate phrase = failwith "You need to implement this function."
+let isLetter = String.filter System.Char.IsLetter
+let getFirstChar (x: string) = x.Substring(0, 1).ToUpper()
+let transform = isLetter >> getFirstChar
+
+let abbreviate (phrase: string) =
+    phrase.Split(" ")
+    |> Array.map (fun (x: string) -> x.Split("-"))
+    |> Seq.concat
+    |> Seq.filter (fun x -> x.Length <> 0)
+    |> Seq.map transform
+    |> String.concat ""
