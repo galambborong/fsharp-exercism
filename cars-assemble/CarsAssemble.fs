c3eeb2eb5b35c1556e41a19aded638c360c47853
Peter Keenan <keenan@posteo.net>
2021-10-08 08:23:44 +0100

    Initial solution for CarsAssemble

--- cars-assemble/CarsAssemble.fs
+++ cars-assemble/CarsAssemble.fs
@@ -1,7 +1,14 @@
 module CarsAssemble
 
-let productionRatePerHour (speed: int): float =
-    failwith "Please implement the 'productionRatePerHour' function"
+let productionRatePerHour speed =
+    let productionWithNoFaults = speed * 221 |> float
+    match speed with
+    | x when x < 5 -> productionWithNoFaults
+    | x when x < 9 -> productionWithNoFaults * 0.9
+    | x when x = 9 -> productionWithNoFaults * 0.8
+    | x when x = 10 -> productionWithNoFaults * 0.77
+    | _ -> failwith "Number not on the scale"
 
-let workingItemsPerMinute(speed: int): int =
-   failwith "Please implement the 'workingItemsPerMinute' function"
+let workingItemsPerMinute speed =
+    let prodRatePerHour = productionRatePerHour speed |> int
+    prodRatePerHour / 60
