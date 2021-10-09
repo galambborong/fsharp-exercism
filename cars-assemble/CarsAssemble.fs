267d616181d1a50cca64d7a461e8ce2927a8c4fd
Peter Keenan <keenan@posteo.net>
2021-10-09 16:09:58 +0100

    Clean up match expressions

--- cars-assemble/CarsAssemble.fs
+++ cars-assemble/CarsAssemble.fs
@@ -2,11 +2,12 @@ module CarsAssemble
 
 let productionRatePerHour speed =
     let productionWithNoFaults = speed * 221 |> float
+
     match speed with
     | x when x < 5 -> productionWithNoFaults
     | x when x < 9 -> productionWithNoFaults * 0.9
-    | x when x = 9 -> productionWithNoFaults * 0.8
-    | x when x = 10 -> productionWithNoFaults * 0.77
+    | 9 -> productionWithNoFaults * 0.8
+    | 10 -> productionWithNoFaults * 0.77
     | _ -> failwith "Number not on the scale"
 
 let workingItemsPerMinute speed =
