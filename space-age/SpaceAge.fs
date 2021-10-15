5c96f4c19385183bd59cb458e12d648a17d9cbd5
Peter Keenan <keenan@posteo.net>
2021-10-15 08:38:23 +0100

    Basic formula sorted

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -13,12 +13,16 @@ type Planet =
     | Neptune
 
 let calculateAge (ageInSeconds: int64) (orbitalValue: float) =
-    let age1 = ageInSeconds |> float |> (*) orbitalValue
-    let yearInSeconds = 31557600.0
-    Math.Round(age1 / yearInSeconds, 2)
-    
+    let ageAsFloat = ageInSeconds |> float //|> (*) orbitalValue
+    let earthYearInSeconds = 31557600.0
+    let age = ageAsFloat / earthYearInSeconds
+    Math.Round(age / orbitalValue, 2)
+
+
+
     // calculateAge 1000000000L 1.0
     // calculateAge 2134835688L 0.2408467
-    
+    // calculateAge 1210123456L 84.016846
+
 
 let age (planet: Planet) (seconds: int64): float = failwith "You need to implement this function."
