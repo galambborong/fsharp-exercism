893d9f74093721b315ce7ae77a81a43b06549060
Peter Keenan <peter.keenan@sage.com>
2021-10-15 08:02:57 +0100

    Some initial SpaceAge logic

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -1,5 +1,7 @@
 ﻿module SpaceAge
 
+open System
+
 type Planet =
     | Mercury
     | Venus
@@ -9,5 +11,14 @@ type Planet =
     | Saturn
     | Uranus
     | Neptune
+
+let calculateAge (ageInSeconds: int64) (orbitalValue: float) =
+    let age1 = ageInSeconds |> float |> (*) orbitalValue
+    let yearInSeconds = 31557600.0
+    Math.Round(age1 / yearInSeconds, 2)
+    
+    // calculateAge 1000000000L 1.0
+    // calculateAge 2134835688L 0.2408467
+    
 
 let age (planet: Planet) (seconds: int64): float = failwith "You need to implement this function."
