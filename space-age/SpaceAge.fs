cb255210fb8dd55a7e20c2f6a7a631a3afed733d
Peter Keenan <keenan@posteo.net>
2021-10-17 22:49:15 +0100

    General refactor and tidy

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -12,26 +12,20 @@ type Planet =
     | Uranus
     | Neptune
 
-let calculateAge (ageInSeconds: int64) (orbitalValue: float) =
-    let ageAsFloat = ageInSeconds |> float //|> (*) orbitalValue
-    let earthYearInSeconds = 31557600.0
-    let age = ageAsFloat / earthYearInSeconds
-    Math.Round(age / orbitalValue, 2)
+let earthYearInSeconds = 60.0 * 60.0 * 24.0 * 365.25
 
+let calculateAge ageInSeconds orbitalPeriod =
+    Math.Round ((float ageInSeconds / earthYearInSeconds) / orbitalPeriod, 2)
 
-    // calculateAge 1000000000L 1.0
-    // calculateAge 2134835688L 0.2408467
-    // calculateAge 1210123456L 84.016846
+let planetOrbitalPeriod planet =
+        match planet with
+        | Mercury -> 0.2408467
+        | Venus -> 0.61519726
+        | Earth -> 1.0
+        | Mars -> 1.8808158
+        | Jupiter -> 11.862615
+        | Saturn -> 29.447498
+        | Uranus -> 84.016846
+        | Neptune -> 164.79132
 
-
-let age (planet: Planet) (seconds: int64): float =
-    let value = match planet with
-                | Mercury -> 0.2408467
-                | Venus -> 0.61519726
-                | Earth -> 1.0
-                | Mars -> 1.8808158
-                | Jupiter -> 11.862615
-                | Saturn -> 29.447498
-                | Uranus -> 84.016846
-                | Neptune -> 164.79132
-    calculateAge seconds value
+let age planet (seconds: int64) = calculateAge seconds (planetOrbitalPeriod planet)
