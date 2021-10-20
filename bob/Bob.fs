87b2677eba75bcf00c9992c114b85ec084a72e2e
Peter Keenan <peter.keenan@sage.com>
2021-10-20 16:38:58 +0100

    Add some logic for Bob

---
+++ bob/Bob.fs
@@ -0,0 +1,14 @@
+﻿module Bob
+
+let response (input: string): string =
+    let trimmedMessage =  input.Trim()
+    let isQuestion = trimmedMessage.EndsWith('?')
+    let isShouting = trimmedMessage = trimmedMessage.ToUpper() && trimmedMessage.Length <> 0
+    let isEmpty = trimmedMessage.Equals("")
+    
+    match isEmpty, isQuestion, isShouting with
+    | true, false, false -> "Fine. Be that way!"
+    | false, true, false -> "Sure."
+    | false, false, true -> "Whoa, chill out!"
+    | false, true, true -> "Calm down, I know what I'm doing!"
+    | _ -> "Whatever."
