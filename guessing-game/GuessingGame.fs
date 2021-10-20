53640c836a40c54e3f49b7d639770665cc32425e
Peter Keenan <peter.keenan@sage.com>
2021-10-20 13:33:21 +0100

    Solution for GuessingGame

--- guessing-game/GuessingGame.fs
+++ guessing-game/GuessingGame.fs
@@ -1,3 +1,9 @@
 module GuessingGame
 
-let reply (guess: int): string = failwith "Please implement the 'reply' function"
+let reply guess =
+    match guess with
+    | 42 -> "Correct"
+    | 41 | 43 -> "So close"
+    | x when x < 41 -> "Too low"
+    | x when x > 43 -> "Too high"
+    | _ -> failwith "This pattern should not be required"
