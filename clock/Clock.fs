a326f72856134d263acb26dc08d757e351d525f8
Peter Keenan <peter.keenan@sage.com>
2021-10-26 10:58:41 +0100

    General refactor/tidy up for Clocks

--- clock/Clock.fs
+++ clock/Clock.fs
@@ -2,48 +2,57 @@ module Clock
 
 open System
 
+let totalMinutesPerDay = TimeSpan(1, 0, 0, 0).TotalMinutes
+
+let numberOfMinutes minutes = TimeSpan(0, minutes, 0).TotalMinutes
+
+let displayNormalTime timeSpan = timeSpan.ToString().Substring(0, 5)
+
+let displayTimeOver24Hrs timeSpan = timeSpan.ToString().Substring(2, 5)
+
 let handleOverHours hours =
     match hours with
     | h when h > 24 -> hours % 24
     | h when h < 24 -> hours
     | h when h = 24 -> 0
-    | _ -> failwith "todo"
-    
-let rec handleNegatives mins =
-    match mins < 0.0 with
-    | true -> handleNegatives (mins + TimeSpan(1, 0, 0, 0).TotalMinutes)
-    | false -> mins
-    
-let rec handleOverMinutes minutes hours =
+    | _ -> failwith "This case should never appear"
+
+let rec handleNegativeMinuteCount minutes =
+    match minutes < 0.0 with
+    | true -> handleNegativeMinuteCount (minutes + totalMinutesPerDay)
+    | false -> minutes
+
+let rec handleExcessMinutes minutes hours =
     match minutes > 60 with
-    | true ->
-        handleOverMinutes (minutes - 60) (hours + 1)
+    | true -> handleExcessMinutes (minutes - 60) (hours + 1)
     | false -> hours, minutes
 
-let create (hours: int) (minutes: int) =
-    let mins = handleOverMinutes minutes hours
-    let x = TimeSpan(handleOverHours (fst mins), snd mins, 0).TotalMinutes
-    x
-    |> handleNegatives
+let create hours minutes =
+    TimeSpan(
+        handleOverHours (fst (handleExcessMinutes minutes hours)),
+        snd (handleExcessMinutes minutes hours),
+        0
+    )
+        .TotalMinutes
+    |> handleNegativeMinuteCount
 
-let add minutes (clock: float) =
-    clock + TimeSpan(0, minutes, 0).TotalMinutes
+let add minutesToAdd currentTotalMinutes =
+    currentTotalMinutes + numberOfMinutes minutesToAdd
 
+let subtract minutesToSubtract currentTotalMinutes =
+    let temporaryMinuteCount =
+        currentTotalMinutes - numberOfMinutes minutesToSubtract
 
+    match temporaryMinuteCount < 0.0 with
+    | true -> handleNegativeMinuteCount temporaryMinuteCount
+    | false -> temporaryMinuteCount
 
-let subtract minutes clock =
-    let tmp = clock - TimeSpan(0, minutes, 0).TotalMinutes
-    match tmp < 0.0 with
-    | true -> handleNegatives tmp
-    | false -> tmp
-
-let realDisplay timeSpan = timeSpan.ToString().Substring(0, 5)
-
-let anotherDisplay timeSpan = timeSpan.ToString().Substring(2, 5)
-
-let display (clock: float) =
-    let time = clock |> handleNegatives |> TimeSpan.FromMinutes
+let display clock =
+    let time =
+        clock
+        |> handleNegativeMinuteCount
+        |> TimeSpan.FromMinutes
 
     match time.Days with
-    | 0 -> realDisplay time
-    | _ -> anotherDisplay time
+    | 0 -> displayNormalTime time
+    | _ -> displayTimeOver24Hrs time
