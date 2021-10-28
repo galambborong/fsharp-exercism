6d1343961f916c3177c0c3df9d9aff92aa21f7b4
Peter Keenan <peter.keenan@sage.com>
2021-10-28 16:49:11 +0100

    Clean up variables and logic in pattern match

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -1,13 +1,13 @@
 ﻿module QueenAttack
 
-let isValidBoardIndex x =
-    [0..7] |> List.contains x
+let isValidBoardIndex x = [ 0 .. 7 ] |> List.contains x
 
 let create (position: int * int) =
-    isValidBoardIndex (fst position) && isValidBoardIndex (snd position)
-    
+    isValidBoardIndex (fst position)
+    && isValidBoardIndex (snd position)
+
 let isSameRowOrColumn x y = x = y
-    
+
 let positionalDifference x y = max x y - min x y
 
 let isDiagonal (x, y) (a, b) =
@@ -15,7 +15,7 @@ let isDiagonal (x, y) (a, b) =
 
 let canAttack (queen1: int * int) (queen2: int * int) =
     match queen1, queen2 with
-    | (x1, _), (x2, _) when isSameRowOrColumn x1 x2 -> true 
-    | (_, y1), (_, y2) when isSameRowOrColumn y1 y2 -> true
-    | (x1, y1), (x2, y2) when positionalDifference x1 x2 = positionalDifference y1 y2 -> true
+    | (q1, _), (q2, _) when isSameRowOrColumn q1 q2 -> true
+    | (_, q1), (_, q2) when isSameRowOrColumn q1 q2 -> true
+    | queen1, queen2 -> isDiagonal queen1 queen2
     | _ -> false
