c91d1aa74bd525ec6e9c8469b14270adf31ecb68
Peter Keenan <peter.keenan@sage.com>
2021-10-28 16:51:12 +0100

    Remove type declarations in function signatures

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -2,7 +2,7 @@
 
 let isValidBoardIndex x = [ 0 .. 7 ] |> List.contains x
 
-let create (position: int * int) =
+let create position =
     isValidBoardIndex (fst position)
     && isValidBoardIndex (snd position)
 
@@ -13,7 +13,7 @@ let positionalDifference x y = max x y - min x y
 let isDiagonal (x, y) (a, b) =
     positionalDifference x a = positionalDifference y b
 
-let canAttack (queen1: int * int) (queen2: int * int) =
+let canAttack queen1 queen2 =
     match queen1, queen2 with
     | (q1, _), (q2, _) when isSameRowOrColumn q1 q2 -> true
     | (_, q1), (_, q2) when isSameRowOrColumn q1 q2 -> true
