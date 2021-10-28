e697032d7aa296d841eec8acfacc3226a69d0797
Peter Keenan <keenan@posteo.net>
2021-10-28 21:54:42 +0100

    Working with DUs

--- robot-simulator/RobotSimulator.fs
+++ robot-simulator/RobotSimulator.fs
@@ -1,9 +1,27 @@
 ﻿module RobotSimulator
 
+open Microsoft.FSharp.Reflection
+
 type Direction = North | East | South | West
 type Position = int * int
 type Robot = { direction: Direction; position: Position }
 
-let create direction position = failwith "You need to implement this function."
 
-let move instructions robot = failwith "You need to implement this function."
+let xxx =
+    FSharpType.GetUnionCases typeof<Direction>
+    |> Array.toList
+    |> List.indexed
+
+let create direction position =
+        { direction = direction; position = position }
+
+let move instructions (robot: Robot) =
+
+    let nextDirection =
+        FSharpType.GetUnionCases typeof<Direction>
+        |> Array.toList
+        |> List.indexed
+
+    match instructions with
+    | "R" -> { direction = Direction. ; position = robot.position }
+    | _ -> failwith "todo"
