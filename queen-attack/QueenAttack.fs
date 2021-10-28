8ed443d0d37dd6aaeda5931f631074a0829e47b2
Peter Keenan <peter.keenan@sage.com>
2021-10-28 16:41:49 +0100

    Abstract some logic into functions

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -1,19 +1,21 @@
 ﻿module QueenAttack
 
 let isValidBoardIndex x =
-    x >= 0 && x < 8
+    [0..7] |> List.contains x
 
 let create (position: int * int) =
     isValidBoardIndex (fst position) && isValidBoardIndex (snd position)
     
-let positionalDifference x y =
-    max x y - min x y
+let isSameRowOrColumn x y = x = y
+    
+let positionalDifference x y = max x y - min x y
+
+let isDiagonal (x, y) (a, b) =
+    positionalDifference x a = positionalDifference y b
 
 let canAttack (queen1: int * int) (queen2: int * int) =
-    let x1, y1 = queen1
-    let x2, y2 = queen2
-    match x1, y1, x2, y2 with
-    | x1, _, x2, _ when x1 = x2 -> true 
-    | _, y1, _, y2 when y1 = y2 -> true
-    | x1, y1, x2, y2 when (positionalDifference x1 x2) = positionalDifference y1 y2 -> true
+    match queen1, queen2 with
+    | (x1, _), (x2, _) when isSameRowOrColumn x1 x2 -> true 
+    | (_, y1), (_, y2) when isSameRowOrColumn y1 y2 -> true
+    | (x1, y1), (x2, y2) when positionalDifference x1 x2 = positionalDifference y1 y2 -> true
     | _ -> false
