d12e23174af938fb69f2655d44c7a6525b135449
Peter Keenan <peter.keenan@sage.com>
2021-10-28 16:02:55 +0100

    First passing solution for QueenAttack

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -1,5 +1,19 @@
 ﻿module QueenAttack
 
-let create (position: int * int) = failwith "You need to implement this function."
+let isValidBoardIndex x =
+    x >= 0 && x < 8
 
-let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
+let create (position: int * int) =
+    isValidBoardIndex (fst position) && isValidBoardIndex (snd position)
+    
+let positionalDifference x y =
+    max x y - min x y
+
+let canAttack (queen1: int * int) (queen2: int * int) =
+    let x1, y1 = queen1
+    let x2, y2 = queen2
+    match x1, y1, x2, y2 with
+    | x1, _, x2, _ when x1 = x2 -> true 
+    | _, y1, _, y2 when y1 = y2 -> true
+    | x1, y1, x2, y2 when (positionalDifference x1 x2) = positionalDifference y1 y2 -> true
+    | _ -> false
