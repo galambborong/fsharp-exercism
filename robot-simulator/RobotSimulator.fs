62c95297ef87617aae45350979048be94f120293
Peter Keenan <keenan@posteo.net>
2021-10-31 21:40:29 +0000

    Renaming and formatting file

--- robot-simulator/RobotSimulator.fs
+++ robot-simulator/RobotSimulator.fs
@@ -1,32 +1,38 @@
 ﻿module RobotSimulator
 
-open System.Net
+type Direction =
+    | North
+    | East
+    | South
+    | West
 
-type Direction = North | East | South | West
 type Position = int * int
-type Robot = { direction: Direction; position: Position }
+
+type Robot =
+    { direction: Direction
+      position: Position }
 
 let create direction position =
-        { direction = direction; position = position }
+    { direction = direction
+      position = position }
 
-let directionsMap robot instruction =
+let mapToDirectionOrPosition robot instruction =
     match robot.direction, robot.position, instruction with
-    | North, _, "R" -> { robot with direction = East } 
-    | North, _, "L" -> { robot with direction = West }
-    | North, (x, y), "A" -> { robot with position =  (x, y + 1)}
-    | East, _, "R" -> { robot with direction = South }
-    | East, _, "L" -> { robot with direction = North }
-    | East, (x, y),"A" -> { robot with position = (x + 1, y) }
-    | South, _, "R" -> { robot with direction = West }
-    | South, _, "L" -> { robot with direction = East }
-    | South, (x, y), "A" -> { robot with position = (x, y - 1)}
-    | West, _, "R" -> { robot with direction = North }
-    | West, _, "L" -> { robot with direction = South }
-    | West, (x, y ), "A" -> { robot with position = (x - 1, y)}
-    | _ -> failwith "todo"
-     
+    | North, _, 'R' -> { robot with direction = East }
+    | North, _, 'L' -> { robot with direction = West }
+    | East, _, 'R' -> { robot with direction = South }
+    | East, _, 'L' -> { robot with direction = North }
+    | South, _, 'R' -> { robot with direction = West }
+    | South, _, 'L' -> { robot with direction = East }
+    | West, _, 'R' -> { robot with direction = North }
+    | West, _, 'L' -> { robot with direction = South }
+    | North, (x, y), 'A' -> { robot with position = (x, y + 1) }
+    | East, (x, y), 'A' -> { robot with position = (x + 1, y) }
+    | South, (x, y), 'A' -> { robot with position = (x, y - 1) }
+    | West, (x, y), 'A' -> { robot with position = (x - 1, y) }
+    | _ -> failwith "Invalid input"
+
 let move instructions robot =
-    let directions = instructions |> Seq.toList |> List.map string
-    
-    directions
-    |> Seq.fold directionsMap robot
+    instructions
+    |> Seq.toList
+    |> Seq.fold mapToDirectionOrPosition robot
