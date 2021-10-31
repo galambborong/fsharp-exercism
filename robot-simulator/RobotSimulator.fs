f860e2734c6d18185ce894ae61d8539504c7d12f
Peter Keenan <keenan@posteo.net>
2021-10-31 11:32:20 +0000

    Work on RobotSimulator

--- robot-simulator/RobotSimulator.fs
+++ robot-simulator/RobotSimulator.fs
@@ -1,7 +1,5 @@
 ﻿module RobotSimulator
 
-open Microsoft.FSharp.Reflection
-
 type Direction = North | East | South | West
 type Position = int * int
 type Robot = { direction: Direction; position: Position }
@@ -9,18 +7,27 @@ type Robot = { direction: Direction; position: Position }
 let create direction position =
         { direction = direction; position = position }
 
-let nextDirection currentDirection instruction =
-    match currentDirection, instruction with
-    | Direction.North, "R" -> Direction.East
-    | Direction.North, "L" -> Direction.West
-    | Direction.North, "A" -> Direction.North
-    | Direction.East, "R" -> Direction.South
-    | Direction.East, "L" -> Direction.North
-    | Direction.East, "A" -> Direction.East
+let directionsMap currentDirection currentPosition instruction =
+    match currentDirection, currentPosition, instruction with
+    | North, position, "R" -> { direction = East; position = position } 
+    | North, position, "L" -> { direction = West; position = position }
+    | North, (x, y), "A" -> { direction = currentDirection; position =  (x, y + 1)}
+    | East, position, "R" -> { direction = South; position = position }
+    | East, position, "L" -> { direction = North; position = position }
+    | East, (x, y),"A" -> { direction = currentDirection; position = (x + 1, y) }
+    | South, position, "R" -> { direction = West; position = position }
+    | South, position, "L" -> {direction = East; position = position }
+    | South, (x, y), "A" -> { direction = South; position = (x, y - 1)}
+    | West, position, "R" -> {direction = North; position = position }
+    | West, position, "L" -> { direction = South; position = position }
+    | West, (x, y ), "A" -> {direction = currentDirection; position = (x - 1, y)}
     | _ -> failwith "todo"
-
-let move instructions (robot: Robot) =
+     
+let move (instructions: string) (robot: Robot) =
+    let directions = instructions |> Seq.toList
     
-    let directionUpdate = nextDirection robot.direction instructions
     
-    { robot with direction = directionUpdate }
+    directions
+    |> Seq.map (fun x -> directionsMap robot.direction robot.position (x.ToString()))
+    
+    // move "RA" (create Direction.North (0, 0))
