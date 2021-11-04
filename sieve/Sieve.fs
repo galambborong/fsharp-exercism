61b6c00632790e8cb0d52b4947f244151a541e94
Peter Keenan <keenan@posteo.net>
2021-11-04 09:05:19 +0000

    Temporary solution

--- sieve/Sieve.fs
+++ sieve/Sieve.fs
@@ -1,3 +1,9 @@
 ﻿module Sieve
-        
+
+let isPrime n =
+    let rec checkPrime i =
+        i > n / 2 || (n % i <> 0 && checkPrime (i + 1))
+    checkPrime 2
+
 let primes limit =
+    seq { for n in 2..limit do if isPrime n then n } |> Seq.toList
