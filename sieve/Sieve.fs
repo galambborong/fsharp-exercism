965513669da8a6443319899a76ae95cf8bb05c77
Peter Keenan <peter.keenan@sage.com>
2021-11-04 15:33:49 +0000

    Working on a recursive approach

--- sieve/Sieve.fs
+++ sieve/Sieve.fs
@@ -6,4 +6,18 @@ let isPrime n =
 
     checkFactors 2
 
-let primes limit = [ 2 .. limit ] |> List.filter isPrime
+let primes' limit = [ 2 .. limit ] |> List.filter isPrime
+
+let primes n =
+    let rec sievePrimes list i =
+        if i < n then
+            list
+            |> List.filter (fun x -> i > x / 2 || x % i <> 0)
+        else
+            sievePrimes list (i + 1)
+    
+    sievePrimes [2..n] 2
+    
+        // primes 10
+        
+        // primes' 13
