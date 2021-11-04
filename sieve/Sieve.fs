ac560dcd0fb0330cc8a1d87f90573961b0a49ef3
Peter Keenan <peter.keenan@sage.com>
2021-11-04 15:47:23 +0000

    Refactored recursive solution with pattern match

--- sieve/Sieve.fs
+++ sieve/Sieve.fs
@@ -1,25 +1,12 @@
 ﻿module Sieve
 
-let isPrime n =
-    let rec checkFactors i =
-        i > n / 2 || n % i <> 0 && checkFactors (i + 1)
-
-    checkFactors 2
-
-let primes' limit = [ 2 .. limit ] |> List.filter isPrime
-
 let primes n =
-    let rec sievePrimes list i =
-        if i < n then
-            let filter = 
-                list
-                |> List.filter (fun x -> i > x / 2 || x % i <> 0)
-            sievePrimes filter (i + 1)
-        else
+    let rec sievePrimes i list =
+        match i < n with
+        | true ->
             list
-    
-    sievePrimes [2..n] 2
-    
-        // primes 10
-        
-        // primes' 13
+            |> List.filter (fun x -> i > x / 2 || x % i <> 0)
+            |> sievePrimes (i + 1)
+        | false -> list
+
+    sievePrimes 2 [ 2 .. n ]
