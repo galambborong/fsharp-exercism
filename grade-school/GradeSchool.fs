3abad09aa17f8af0240b9d084ab861f5be38fb1d
Peter Keenan <peter.keenan@sage.com>
2021-11-05 15:01:54 +0000

    Playing with GradeSchool

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -2,9 +2,20 @@
 
 type School = Map<int, string list>
 
-let empty: School = failwith "You need to implement this function."
+let empty: School = Map.empty
 
-let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
+let school = Map.empty
+
+let add (student: string) (grade: int) (school: School): School =
+    let list = match school.ContainsKey(grade) with
+                | true -> school.[grade] @ [student]
+                | false -> List.empty)
+    
+    match school.ContainsKey(grade) with
+    | false -> school.Add(grade, [student])
+    | true -> school.Add(grade, list)
+    
+    // add "pK" 2 school
 
 let roster (school: School): string list = failwith "You need to implement this function."
 
