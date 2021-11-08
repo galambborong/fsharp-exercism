7af54deb0cb8f5a745ce712d6225850d8bb9dfda
Peter Keenan <keenan@posteo.net>
2021-11-08 19:38:41 +0000

    Map.values not working

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -4,15 +4,13 @@ type School = Map<int, string list>
 
 let empty: School = Map []
 
-let add (student: string) (grade: int) (school: School) =
+let add student grade (school: School) : School =
+    // TODO Check this works without pattern match
     match school.TryFind grade with
-    | None -> school.Add(grade, [student])
-    | Some currentGradeStudents -> school.Add(grade, currentGradeStudents @ [student])
-    
-    
-    
-// add "p" 1
+    | None -> school.Add(grade, [ student ])
+    | Some currentGradeStudents -> school.Add(grade, currentGradeStudents @ [ student ])
 
-let roster (school: School): string list = failwith "You need to implement this function."
+let roster (school: School) : string list = school |> Map.values
 
-let grade (number: int) (school: School): string list = failwith "You need to implement this function."
+let grade (number: int) (school: School) : string list =
+    failwith "You need to implement this function."
