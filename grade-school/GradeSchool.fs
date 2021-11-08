9af3de08d6eaca24726531107007bdb6ecdb2ae2
Peter Keenan <keenan@posteo.net>
2021-11-08 18:33:26 +0000

    Solution for add

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -5,15 +5,13 @@ type School = Map<int, string list>
 let empty: School = Map []
 
 let add (student: string) (grade: int) (school: School) =
-    let gradeExists = school |> Map.tryFind grade
-    
-    match gradeExists with
+    match school.TryFind grade with
     | None -> school.Add(grade, [student])
-    | Some _ -> school
+    | Some currentGradeStudents -> school.Add(grade, currentGradeStudents @ [student])
     
     
     
-// add "p" 1 a
+// add "p" 1
 
 let roster (school: School): string list = failwith "You need to implement this function."
 
