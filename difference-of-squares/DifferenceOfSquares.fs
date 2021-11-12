4ed30d010f20b293e4495fe3f71dd237a0900288
Peter Keenan <keenan@posteo.net>
2021-11-12 14:32:12 +0000

    Apply fantomas to DiffOrSquares

--- difference-of-squares/DifferenceOfSquares.fs
+++ difference-of-squares/DifferenceOfSquares.fs
@@ -2,14 +2,10 @@
 
 let square n = n * n
 
-let squareOfSum number =
-    [1..number]
-    |> List.sum
-    |> square
+let squareOfSum number = [ 1 .. number ] |> List.sum |> square
 
-let sumOfSquares number =
-    [1..number]
-    |> List.sumBy square
+let sumOfSquares number = [ 1 .. number ] |> List.sumBy square
 
 let differenceOfSquares number =
-    max (squareOfSum number) (sumOfSquares number) - min (squareOfSum number) (sumOfSquares number)
+    max (squareOfSum number) (sumOfSquares number)
+    - min (squareOfSum number) (sumOfSquares number)
