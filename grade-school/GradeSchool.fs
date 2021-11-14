2d272abaf71bc95a2d056f78ab7b15b74d415583
Peter Keenan <keenan@posteo.net>
2021-11-14 22:21:10 +0000

    General refactor of GradeSchool

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -5,16 +5,16 @@ type School = Map<int, string list>
 let empty: School = Map []
 
 let add student grade (school: School) =
-    match school.TryFind grade with
+    match grade |> school.TryFind with
     | Some currentStudents -> school.Add(grade, currentStudents @ [ student ])
     | None -> school.Add(grade, [ student ])
 
-let roster (school: School) =
+let roster school =
     school
     |> Map.toList
     |> List.collect (snd >> List.sort)
 
 let grade number (school: School) =
-    match school.TryFind number with
-    | Some studentsInGrade -> List.sort studentsInGrade
+    match number |> school.TryFind with
+    | Some studentsInGrade -> studentsInGrade |> List.sort
     | None -> List.empty
