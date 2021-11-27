b31d7b36bfc5c9e0519d5c4e7425aeb352c26312
Peter Keenan <keenan@posteo.net>
2021-11-27 10:23:42 +0000

    Some bad logic for BinarySearch

--- binary-search/BinarySearch.fs
+++ binary-search/BinarySearch.fs
@@ -1,3 +1,31 @@
 ﻿module BinarySearch
 
-let find input value = failwith "You need to implement this function."
+let x = [|1;2;3|]
+
+let find (input: int[]) (value: int) =
+    let inputLength = input.Length
+    let midWayIndex = inputLength / 2
+    
+    let x = input.[midWayIndex]
+    
+    let rec doTheFind currentX currentMidWay value =
+        match currentX, value with
+        | x, y when x = y -> Some currentMidWay
+        | x, y when x > y ->
+            let newMidWayIndex = midWayIndex / 2
+            let newX = input.[newMidWayIndex]
+            doTheFind newX newMidWayIndex value
+        | x, y when x < y ->
+            let newMidWayIndex = (currentMidWay + (currentMidWay / 2))
+            let newX = input.[newMidWayIndex]
+            doTheFind newX newMidWayIndex value
+        | _ -> None
+        
+    doTheFind x midWayIndex value
+        
+        
+// find [|1; 2; 3|] 2
+    
+        
+    
+    
