3c3ce07276df21458c029c10377f0b2e3810742c
Peter Keenan <keenan@posteo.net>
2021-11-27 15:42:06 +0000

    Minor changes and add TODO

--- binary-search/BinarySearch.fs
+++ binary-search/BinarySearch.fs
@@ -7,32 +7,39 @@ let x = [|1;2;3|]
 let calculateHalf i = Math.Floor(i / 2 |> float) |> int
 
 let find (input: int[]) (value: int) =
-    let n = input.Length
-    let i = calculateHalf n
-    let x = value
+    let length = input.Length
+    let initialIndex = calculateHalf length
     
     let rec checkIndex i x =
+        printf $"i: {i} :: x: {x}\n"
+        printf $"value at index {i} = {input.[i]}\n"
+        printf $"{input.[i] = x}\n"
+        
+        // TODO We need to confirm that the next index +/- has been checked already
+        
         match input.[i] with
-        | idx when idx = x -> Some i
-        | idx when idx > x ->
+        | valueAtIndex when valueAtIndex = x -> Some i
+        | valueAtIndex when valueAtIndex > x ->
             let nextI = calculateHalf i
             match nextI with
-            | idx when idx < 0 -> None
+            | nextI when nextI < 0 -> None
             | _ -> checkIndex nextI x
-        | idx when idx < x ->
+        | valueAtIndex when valueAtIndex < x ->
             let nextI = calculateHalf i
-            match nextI, n with
-            | idx, n when idx < n -> None
-            | idx, n when (idx + i) > n -> None
+            match nextI with
+//            | nextI when nextI > length -> None
+            | nextI when (nextI + i) > length -> None
             | _ -> checkIndex (i + nextI) x
-        | _ -> None
+        | _ -> None 
     
     match input |> Array.isEmpty with
     | true -> None
-    | false -> checkIndex i x
+    | false -> checkIndex initialIndex value
         
 // find [|1; 2; 3|] 2
 // find [|6|] 6
+
+// find [|1;3;4;6;8;9;11|] 11
     
         
     
