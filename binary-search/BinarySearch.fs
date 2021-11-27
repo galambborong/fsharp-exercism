3505920cc908dafa0f8f11c4cee253bc4ff49aa3
Peter Keenan <keenan@posteo.net>
2021-11-27 15:22:46 +0000

    Some tests passing but logic not suitable

--- binary-search/BinarySearch.fs
+++ binary-search/BinarySearch.fs
@@ -1,30 +1,38 @@
 ﻿module BinarySearch
 
+open System
+
 let x = [|1;2;3|]
 
+let calculateHalf i = Math.Floor(i / 2 |> float) |> int
+
 let find (input: int[]) (value: int) =
-    let inputLength = input.Length
-    let midWayIndex = inputLength / 2
+    let n = input.Length
+    let i = calculateHalf n
+    let x = value
     
-    let x = input.[midWayIndex]
-    
-    let rec doTheFind currentX currentMidWay value =
-        match currentX, value with
-        | x, y when x = y -> Some currentMidWay
-        | x, y when x > y ->
-            let newMidWayIndex = midWayIndex / 2
-            let newX = input.[newMidWayIndex]
-            doTheFind newX newMidWayIndex value
-        | x, y when x < y ->
-            let newMidWayIndex = (currentMidWay + (currentMidWay / 2))
-            let newX = input.[newMidWayIndex]
-            doTheFind newX newMidWayIndex value
+    let rec checkIndex i x =
+        match input.[i] with
+        | idx when idx = x -> Some i
+        | idx when idx > x ->
+            let nextI = calculateHalf i
+            match nextI with
+            | idx when idx < 0 -> None
+            | _ -> checkIndex nextI x
+        | idx when idx < x ->
+            let nextI = calculateHalf i
+            match nextI, n with
+            | idx, n when idx < n -> None
+            | idx, n when (idx + i) > n -> None
+            | _ -> checkIndex (i + nextI) x
         | _ -> None
-        
-    doTheFind x midWayIndex value
-        
+    
+    match input |> Array.isEmpty with
+    | true -> None
+    | false -> checkIndex i x
         
 // find [|1; 2; 3|] 2
+// find [|6|] 6
     
         
     
